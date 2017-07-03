af89b7318ba9985e51348d4c5ddb216a91eddd8b
Aseem Sood <asood123@users.noreply.github.com>
2017-07-03 14:09:53 -0400

    https server for localhost

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import './lib/load-dot-env'; // important to load first for environment config
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
 import routes from './routes';
 import os from 'os';
 import expressLib from './lib/express';
@@ -24,5 +26,12 @@ const server = app.listen(port, () => {
 });
 
 server.timeout = 15000; // sets timeout to 15 seconds
+
+// https server needed for testing apple pay
+const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
+https.createServer(credentials, app).listen(8460);
 
 export default app;
